6779454028998a608a7a00c86270be97b6e2a755
Ionuț Arțăriși <ionut@artarisi.eu>
2016-02-14 20:58:41 +0000

    Put it on pypi

---
+++ setup.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+from undecorated import __version__
+
+
+with open('README.rst') as readme:
+    long_description = readme.read()
+
+
+setup(
+    name='undecorated',
+    version=__version__,
+    author='Ionuț Arțăriși',
+    author_email='ionut@artarisi.eu',
+    description='Undecorate python functions or methods',
+    long_description=long_description,
+    url='https://github.com/mapleoin/undecorated',
+    py_modules=['undecorated'],
+    install_requires=[],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Programming Language :: Python'
+    ],
+    package_data={
+        'undecorated': [
+        ]
+    }
+)

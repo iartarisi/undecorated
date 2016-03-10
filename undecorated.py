641434ef0d1056fecdedbe7dacfe2d915b89408b
Ionuț Arțăriși <ionut@artarisi.eu>
2016-03-10 21:47:17 +0000

    Remove module docstring as we have it on the func

--- undecorated.py
+++ undecorated.py
@@ -13,8 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Return a function with any decorators removed """
-
 
 __version__ = '0.1.1'
 
